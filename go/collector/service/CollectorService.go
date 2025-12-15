aa3cb8d8dee269c6310a5798aa32b86f04615d56
Sharon Aicler <saichler@gmail.com>
2025-12-15 07:20:32 -0800

    Add log

--- go/collector/service/CollectorService.go
+++ go/collector/service/CollectorService.go
@@ -16,6 +16,7 @@ type CollectorService struct {
 
 func Activate(linksID string, vnic ifs.IVNic) {
 	collServiceName, collServiceArea := targets.Links.Collector(linksID)
+	vnic.Resources().Logger().Info("Starting Collector on ", collServiceArea, " area ", collServiceArea)
 	sla := ifs.NewServiceLevelAgreement(&CollectorService{}, collServiceName, collServiceArea, true, nil)
 	vnic.Resources().Services().Activate(sla, vnic)
 }
